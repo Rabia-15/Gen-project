8299758483ca71397f73baa5db18aad12f425423
Rabia-15 <15rabia2003@gmail.com>
2025-04-23 12:47:23 +0530

    Update video_summary.py

--- video_summary.py
+++ video_summary.py
@@ -44,19 +44,19 @@ def extract_frames(video_path, interval_seconds=5):
   for file in os.listdir(frames_directory):
     os.remove(os.path.join(frames_directory,file))
 
-video = cv2.VideoCapture(video_path)
-fps = int(video.get(cv2, CAP_PROP_FRAME_COUNT))
-frames_count = int(video.get(cv2, CAP_PROP_FRAME_COUNT))
+  video = cv2.VideoCapture(video_path)
+  fps = int(video.get(cv2, CAP_PROP_FRAME_COUNT))
+  frames_count = int(video.get(cv2, CAP_PROP_FRAME_COUNT))
 
-current_frame = 0
-frame_number = 1
+  current_frame = 0
+  frame_number = 1
 
-while current_frame <= frame_count:
-  video.set(cv2, CAP_PROP_POS_FRAMES, current_frame)
-  success, frame = video.read()
-  if not success:
-    current_frame += fps * interval_seconds
-    continue
+  while current_frame <= frame_count:
+    video.set(cv2, CAP_PROP_POS_FRAMES, current_frame)
+    success, frame = video.read()
+    if not success:
+     current_frame += fps * interval_seconds
+     continue
 
   frame_path = os.path.join(frames_directory, f"frame_{frame_number:03d}.jpg")
   cv2.imwrite(frame_path, frame)
